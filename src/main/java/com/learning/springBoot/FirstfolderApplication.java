239839e4a8dc6e3068f448d2d5e5697ffbd6acea
JPCaillot <joaopcaillot@gmail.com>
2023-04-24 16:49:28 -0300

    Initial commit

---
+++ src/main/java/com/learning/springBoot/FirstfolderApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstfolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstfolderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/firstfolder/FirstfolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.firstfolder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstfolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
